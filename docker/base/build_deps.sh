74310a5a0385d899c6fec1dcd96e169243ebc88c
Jaume Martin <jaumartin@gmail.com>
2018-02-12 23:53:53 +0100

    Added case for building Yara.

--- docker/base/build_deps.sh
+++ docker/base/build_deps.sh
@@ -16,11 +16,33 @@ rm -rf /deps-build && cp -r $1 /deps-build
 
 # Build all the dependencies (no order for now)
 for dep in `ls /deps-build`; do
-	echo "Configuring dependency $dep for $HOST..."
-	(cd /deps-build/$dep && ./configure --disable-shared --host=$HOST --prefix=$PREFIX --silent ${@:2})
+    if [[ "$dep" == *"yara"* ]]; then
+        if [ -f "/usr/local/lib/libyara.so" ] || [ -f "/usr/local/lib/libyara.a" ]; then
+            echo "Removing previous stuff for dependency $dep for $HOST..."
+            # (cd /deps-build/$dep && make uninstall)
+            (rm -rf "/usr/local/include/yara")
+            (rm -rf "/usr/local/include/yara.h")
+            (rm -rf "/usr/local/lib/pkgconfig/yara.pc")
+            (rm -rf "/usr/local/lib/libyara*")
+            (rm -rf "/usr/local/bin/yara*")
+        fi
 
-	echo "Building dependency $dep for $HOST..."
-	(cd /deps-build/$dep && make --silent -j install)
+        echo "Bootstaping dependency $dep for $HOST..."
+        (cd /deps-build/$dep && ./bootstrap.sh)
+
+        echo "Configuring dependency $dep for $HOST..."
+        (cd /deps-build/$dep && ./configure --disable-shared --host=$HOST --disable-magic --disable-cuckoo --without-crypto --prefix=$PREFIX)
+
+        echo "Building dependency $dep for $HOST..."
+        (cd /deps-build/$dep && make && make install)
+
+    else
+        echo "Configuring dependency $dep for $HOST..."
+        (cd /deps-build/$dep && ./configure --disable-shared --host=$HOST --prefix=$PREFIX --silent ${@:2})
+
+        echo "Building dependency $dep for $HOST..."
+        (cd /deps-build/$dep && make --silent -j install)
+    fi
 done
 
 # Remove any build artifacts
